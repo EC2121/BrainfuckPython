9d65ef093d1025dca5a3ae891cba08d06e78b800
EC2121 <eliacantinipp@gmail.com>
2022-10-17 17:49:05 +0200

    Update brainfuck.py

--- brainfuck.py
+++ brainfuck.py
@@ -10,71 +10,142 @@ class BrainfuckMachine():
     def __init__(self,size):
         self.size = size
         self.head = 0
+        self.index = 0
+        self.instructions = {
+            '+' : self.plus,
+            '-' : self.minus,
+            '>' : self.move_head_to_right,
+            '<' : self.move_head_to_left,
+            '[' : self.open_bracket,
+            ']' : self.closed_bracket,
+            '.' : self.print_code
+        }
         self.tape = [0] * size  
         self.code = ""
         self.message = ""
     
+    
+    
+    def plus(self):
+        self.tape[self.head] += 1
+        if self.tape[self.head] == 255:
+            self.tape[self.head] = 0
+            
+    def minus(self):
+        self.tape[self.head] -= 1
+        if self.tape[self.head] == -1:
+            self.tape[self.head] = 255
+    
+    def move_head_to_right(self):
+        self.head += 1
+        if self.head >= self.size:
+            raise self.HeadOverflow
+    
+    def move_head_to_left(self):
+        self.head -= 1
+        if self.head < 0:
+            raise self.HeadOverflow
+        
+    def open_bracket(self):
+        if self.tape[self.head] == 0:
+            same_bracket_counter = 1
+            while same_bracket_counter > 0:
+                self.index += 1
+                if self.index >= len(self.code):
+                     raise self.BracketMismatch
+                if self.code[self.index] == ']':
+                    same_bracket_counter -= 1
+                elif self.code[self.index] == '[':
+                    same_bracket_counter += 1 
+    
+    
+    def closed_bracket(self):
+        if self.tape[self.head] != 0:
+            same_bracket_counter = 1                
+            while same_bracket_counter > 0:
+                self.index -= 1
+                if self.index < 0:
+                    raise self.BracketMismatch
+                if self.code[self.index] == ']':
+                    same_bracket_counter += 1
+                elif self.code[self.index] == '[':
+                    same_bracket_counter -= 1      
+    
+    
+    def print_code(self):
+        self.message = chr(self.tape[self.head])   #[chr(x) for x in self.tape if x != 0] 
+        print(self.message)
+        
+        
+    
     def run(self):
         
-        i = 0
+       
         
-        while i < len(self.code):
+        while self.index < len(self.code):
             
-            if self.code[i] == '+':
-                if self.tape[self.head] == 255:
-                    self.tape[self.head] = 0
-                else:
-                    self.tape[self.head] += 1 
+            instruction = self.instructions[self.code[self.index]]
+            instruction()
+            self.index += 1
+            
+            
+            
+            
+            
+            # if self.code[self.index] == '+':
+            #     if self.tape[self.head] == 255:
+            #         self.tape[self.head] = 0
+            #     else:
+            #         self.tape[self.head] += 1 
                 
-            elif self.code[i] == '-':
-                if self.tape[self.head] == 0:
-                    self.tape[self.head] = 255
-                else:
-                    self.tape[self.head] -= 1 
+            # elif self.code[self.index] == '-':
+            #     if self.tape[self.head] == 0:
+            #         self.tape[self.head] = 255
+            #     else:
+            #         self.tape[self.head] -= 1 
             
-            elif self.code[i] == '<':
-                self.head -= 1
-                if self.head < 0:
-                    raise self.HeadOverflow
+            # elif self.code[self.index] == '<':
+            #     self.head -= 1
+            #     if self.head < 0:
+            #         raise self.HeadOverflow
                 
-            elif self.code[i] == '>':
-                self.head += 1
-                if self.head > self.size:
-                    raise self.HeadOverflow
+            # elif self.code[self.index] == '>':
+            #     self.head += 1
+            #     if self.head > self.size:
+            #         raise self.HeadOverflow
             
-            elif self.code[i] == '[':
+            # elif self.code[self.index] == '[':
                 
-                if self.tape[self.head] == 0:
-                    same_bracket_counter = 1
-                    while same_bracket_counter > 0:
-                        i += 1
-                        if i >= len(self.code):
-                            raise self.BracketMismatch
-                        if self.code[i] == ']':
-                            same_bracket_counter -= 1
-                        elif self.code[i] == '[':
-                            same_bracket_counter += 1
+            #     if self.tape[self.head] == 0:
+            #         same_bracket_counter = 1
+            #         while same_bracket_counter > 0:
+            #             i += 1
+            #             if i >= len(self.code):
+            #                 raise self.BracketMismatch
+            #             if self.code[i] == ']':
+            #                 same_bracket_counter -= 1
+            #             elif self.code[i] == '[':
+            #                 same_bracket_counter += 1
             
             
-            elif self.code[i] == ']':
-                if self.tape[self.head] != 0:
-                    same_bracket_counter = 1                
-                    while same_bracket_counter > 0:
-                        i -= 1
-                        if i < 0:
-                            raise self.BracketMismatch
-                        if self.code[i] == ']':
-                            same_bracket_counter += 1
-                        elif self.code[i] == '[':
-                           same_bracket_counter -= 1      
+            # elif self.code[i] == ']':
+            #     if self.tape[self.head] != 0:
+            #         same_bracket_counter = 1                
+            #         while same_bracket_counter > 0:
+            #             i -= 1
+            #             if i < 0:
+            #                 raise self.BracketMismatch
+            #             if self.code[i] == ']':
+            #                 same_bracket_counter += 1
+            #             elif self.code[i] == '[':
+            #                same_bracket_counter -= 1      
             
             
-            elif self.code[i] == '.':
-                self.message = chr(self.tape[self.head])   #[chr(x) for x in self.tape if x != 0] 
-                print(self.message)
+            # elif self.code[i] == '.':
+            #     self.message = chr(self.tape[self.head])   #[chr(x) for x in self.tape if x != 0] 
+            #     print(self.message)
                         
                    
-            i += 1      
                         
             
          
